761228a816ade8b12c630294dbe59fc92e0a0619
J. Derek Tucker <jdtuck@sandia.gov>
2026-05-06 11:36:57 -0600

    bump version

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from setuptools import setup
 
 setup(
     name='pyBayesPPR',
-    version='1.0.0',
+    version='1.0.1',
     description='Bayesian Projection Pursuit Regression',
     url='https://github.com/gqcollins/pyBayesPPR.git',
     author='Gavin Collins',
-    author_email='',
+    author_email='gqcolli@sandia.gov',
     license='BSD-3',
     packages=setuptools.find_packages(),
     zip_safe=False,
-    python_requires='>=3.6',
+    python_requires='>=3.9',
     install_requires=['numpy', 'scipy']
 )
